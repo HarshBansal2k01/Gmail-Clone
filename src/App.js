8cc63d4a1cf580c676ad217ca8f8075630714090
HarshBansal2k01 <ihbbro14@gmail.com>
2021-12-06 15:59:19 +0530

    done with section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from "react";
 import "./App.css";
-import Header from "./Header"
+import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Mail from "./Mail";
+import EmailList from "./EmailList"
 
 function App() {
-  return <div className="App">
-    <Header/>
-    <Sidebar/>
-  </div>;
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+
+          <Routes>
+            <Route path="/mail" element={ <Mail />}/>
+            <Route path="/" element={ <EmailList />}/>
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
+
+
+
+// In react-router-dom v6, "Switch" is replaced by routes "Routes".
